d740d2effd800aea5f1b68df539bad1fbbb8b887
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2018-12-09 19:27:34 +0700

    add touchpad setting

--- setup.sh
+++ setup.sh
@@ -87,6 +87,12 @@ defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 # @TOCHECK
 defaults write NSGlobalDomain com.apple.trackpad.forceClick -bool false
 
+# Enable tap to click
+defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
+sudo defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+sudo defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+sudo defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
 # Don’t animate opening applications from the Dock
 defaults write com.apple.dock launchanim -bool false
 
